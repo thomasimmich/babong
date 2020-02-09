463e6eeaca8881778d1626269657ccbed6bc7f12
thomasimmich <thomas.immich@centigrade.de>
2020-02-09 15:20:39 +0100

    Added controlling via fire

--- main.ts
+++ main.ts
@@ -1,5 +1,39 @@
+namespace SpriteKind {
+    export const Effect = SpriteKind.create()
+}
+namespace myTiles {
+    //% blockIdentity=images._tile
+    export const tile0 = img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`
+}
+controller.up.onEvent(ControllerButtonEvent.Released, function () {
+    isFiring = true
+})
+controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
+    balloon.y += 1
+})
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
-    fire = sprites.create(img`
+    balloon.y += -1
+})
+let balloon: Sprite = null
+let isFiring = false
+let fire = sprites.create(img`
 . . . 2 . . . . 
 . . . . . . . . 
 . . 2 . 2 . 4 . 
@@ -12,32 +46,21 @@ controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
 . 2 4 5 5 4 2 . 
 . 2 5 5 4 2 . . 
 . . 2 4 2 . . . 
-`, SpriteKind.Player)
-    fire.startEffect(effects.fountain, 200)
-})
-controller.A.onEvent(ControllerButtonEvent.Released, function () {
-    fire.setImage(img`
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-`)
-})
-let fire: Sprite = null
+`, SpriteKind.Effect)
+isFiring = false
 scene.setBackgroundColor(9)
-let balloon = sprites.create(img`
+let smallMountain = sprites.create(img`
+. . . . . . . 1 1 . . . . . . . 
+. . . . . . 1 1 1 1 . . . . . . 
+. . . . . 1 1 1 1 1 1 . . . . . 
+. . . . 1 1 1 1 1 1 1 1 . . . . 
+. . . 1 1 1 1 b b b 1 1 1 . . . 
+. . 1 1 b b b b b b b 1 1 1 . . 
+. b 1 b b b b b b b b b b b b . 
+b b b b b b b b b b b b b b b b 
+`, SpriteKind.Player)
+smallMountain.setPosition(160, 116)
+balloon = sprites.create(img`
 . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
 . . . . . . . . . . 6 6 6 6 6 6 1 1 1 1 2 2 2 2 4 4 1 1 1 . . . . . . . . . 
 . . . . . . . . 6 f 6 1 6 1 6 1 1 1 1 1 1 1 2 2 2 4 4 1 1 1 1 . . . . . . . 
@@ -114,7 +137,49 @@ d d d 1 d 1 d 1 d 1 d 1 d 1 d 1 d 1 d d d 1 d 1 d 1 d 1 d d
 . . d d d d d d . . . d d d d d d d . . . . . . . . . . . . 
 . . . . . . . . . . . . d d d d d . . . . . . . . . . . . . 
 `, SpriteKind.Enemy)
-bigCloud.setPosition(121, 22)
+bigCloud.setPosition(123, 22)
+let smallCloud = sprites.create(img`
+. . . . . . . . . d d d d d d . . . . . . . . . 
+. . . . . . . . d d 1 1 1 1 d d . . . . . . . . 
+. . . . . . d d d 1 d 1 1 1 1 1 d . . . . . . . 
+. . . . . d 1 d 1 d 1 1 1 1 1 1 1 d d . . . . . 
+. . . . d 1 d 1 d 1 d 1 d 1 1 1 1 1 1 d d . . . 
+. . . d 1 d 1 d 1 d 1 d 1 d 1 1 1 1 1 1 d . . . 
+. . d d d 1 d 1 d 1 d 1 d 1 d 1 d 1 1 1 1 d . . 
+. d 1 d d d 1 d 1 d 1 d 1 d 1 d 1 d 1 1 1 d . . 
+d 1 d 1 d 1 d 1 d 1 d 1 d 1 d 1 d 1 d 1 1 1 d . 
+d d 1 d 1 d 1 d 1 d 1 d 1 d 1 d 1 d 1 d 1 d d d 
+d d d 1 d 1 d 1 d 1 d 1 d d d 1 d 1 d 1 d 1 d d 
+d d d d 1 d 1 d 1 d 1 d d d d d 1 d 1 d 1 d d d 
+. d d 1 d d d d d 1 d d d d d d d d d d d d d . 
+. d d d d d d d 1 d d d d d d d d d d d d d d . 
+. . d d d d d d d d . d d d d d d d . . . . . . 
+. . . . . . . . . . . . d d d d d . . . . . . . 
+`, SpriteKind.Enemy)
+let fog = sprites.create(img`
+. 1 1 . . . . . 1 . . . 1 . . 1 . . . . 1 . . 1 . . . . . 1 . . . . . . . . 1 . . 1 . . . . . . . . . . . . . 1 1 1 1 1 1 . . . . . 1 1 . 1 . 1 1 1 . . . . 1 . . . . . 1 1 . . . 1 . 1 . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
+1 . . . 1 . . . . . 1 . . . 1 . . 1 1 . 1 . 1 . 1 1 1 . . . . 1 . 1 . . . . . . 1 . . . 1 . . . 1 1 1 1 1 1 1 . . . . . . . . . . . . . . 1 1 1 . . . . 1 . . . . 1 . . . . . 1 . . . 1 . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
+`, SpriteKind.Player)
+fog.setPosition(123, 22)
 game.onUpdateInterval(100, function () {
     bigCloud.x += -1
+})
+game.onUpdateInterval(200, function () {
+    balloon.y += 1
+    smallMountain.x += -1
+    isFiring = false
+})
+game.onUpdateInterval(30, function () {
+    fog.x += -1
+})
+forever(function () {
+    fire.setPosition(balloon.x, balloon.y)
+    fire.y += 16
+    if (isFiring) {
+        fire.startEffect(effects.fire, 100)
+        balloon.y += -1
+    }
+})
+game.onUpdateInterval(50, function () {
+    smallCloud.x += -1
 })
